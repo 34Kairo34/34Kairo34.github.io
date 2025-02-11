eda7ee336c3fae42501077de6637a609d6d2b747
34Kairo34 <nabefan77@gmail.com>
2025-02-11 01:54:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
     animeItems.forEach(item => {
         item.addEventListener('click', () => {
-            // Here you can define the behavior when an anime item is clicked, e.g., show more details.
             alert('You clicked on ' + item.querySelector('h3').innerText);
         });
     });
