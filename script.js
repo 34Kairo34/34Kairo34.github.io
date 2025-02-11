f74f24724c5db026b0a6ae0189823e9529a2a4b3
34Kairo34 <nabefan77@gmail.com>
2025-02-11 01:57:49 -0800

    Update and rename script.js to scripts.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const animeItems = document.querySelectorAll('.anime-item');
-    
-    animeItems.forEach(item => {
-        item.addEventListener('click', () => {
-            alert('You clicked on ' + item.querySelector('h3').innerText);
-        });
-    });
-});
